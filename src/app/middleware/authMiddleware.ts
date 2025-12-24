9a5174bbd5e6ce5758f10218f43f8dfda187d128
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-12-24 19:10:34 +0600

    authmiddleware and restricto role fn done

--- src/app/middleware/authMiddleware.ts
+++ src/app/middleware/authMiddleware.ts
@@ -1,3 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from '../modules/auth/authService';
 
+export const protect= async(req:Request, res:Response, next:NextFunction) => {
+try {
+    let token=req.headers.authorization?.startsWith('Bearer ') ? req.headers.authorization.split(' ')[1] : null;
+    if(!token){
+        return res.status(401).json({success:false, message:'Not authorized, no token'});
+    }
+    const decoded= AuthService.verifyToken(token);
+    req.user={ id: decoded.id,role: decoded.role };
+    next();
+} catch (error) {
+    next(error);
+}
+}
 
-
+export const restrictTo = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return next(new Error('You do not have permission to perform this action'));
+    }
+    next();
+  };
+};
