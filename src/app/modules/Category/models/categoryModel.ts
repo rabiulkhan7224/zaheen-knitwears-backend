fd37d63ebfa3cf1811f48fdad805768841ad23c2
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-12-27 18:04:27 +0600

    category model added

---
+++ src/app/modules/Category/models/categoryModel.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface ICategory extends Document {
+  name: string;
+  description?: string;
+  image?: string;
+  slug: string;
+}
+
+const categorySchema = new Schema<ICategory>(
+  {
+    name: { type: String, required: true, unique: true, trim: true },
+    description: { type: String },
+    image: { type: String },
+    slug: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+export const Category = model<ICategory>('Category', categorySchema);
